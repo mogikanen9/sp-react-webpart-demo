5736023a45ae5597ff3849fd3d029e68e37404c8
Vlad Voinov <mogikanensoftware@gmail.com>
2018-03-22 16:06:28 -0400

    init skeleton for ViewList uni test

---
+++ src/webparts/helloBook/components/list/ViewList.test.tsx
@@ -0,0 +1,10 @@
+import * as mocha from 'mocha';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+
+import { ViewList } from './ViewList';
+import { IViewListProps } from './IViewListProps';
+
+describe('ViewList tests',()=>{
+
+});
