d688e8ab37ecbd3ebba3b4303fdf19f9841e4b0d
Vlad Voinov <mogikanensoftware@gmail.com>
2018-04-06 10:41:34 -0400

    service call adn link to home added to BookCRUD form

--- src/webparts/helloBook/components/forms/BookCRUD.tsx
+++ src/webparts/helloBook/components/forms/BookCRUD.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { IBookCRUDProps } from './IBookCRUDProps';
+import { Book } from '../../service/vo/Book';
+import { Link } from 'react-router-dom';
 
 class BookCRUD extends React.Component<IBookCRUDProps> {
 
@@ -7,8 +9,21 @@ class BookCRUD extends React.Component<IBookCRUDProps> {
         super();
     }
 
+    public componentDidMount() {
+        this.props.bookService.getById(this.props.itemId).then((book: Book) => {
+            console.log('loaded book->', book);
+        }).catch((err) => {
+            console.error(err);
+        });
+    }
+
     public render() {
-        return (<div>Book CRUD for item {this.props.itemId}</div>);
+        return (<div>
+            <h3>Book CRUD for item {this.props.itemId}</h3>
+            <p>
+                <Link to="/home">Home</Link>
+            </p>
+        </div>);
     }
 }
 
